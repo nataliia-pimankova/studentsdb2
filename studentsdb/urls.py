51989fb5d669b8d757ffecbbd1e776999b5e630b
natalya2487 <natalya2487@gmail.com>
2016-10-05 13:47:28 +0300

    add students_list

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -20,6 +20,15 @@ from students import views
 urlpatterns = [
     # Students urls
     url(r'^$', views.students_list, name='home'),
+    url(r'^students/add/$', views.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name='students_delete'),
+
+    # Groups urls
+    url(r'^groups/$', views.groups_list, name='groups'),
+    url(r'^groups/add/$', views.groups_add, name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', views.groups_edit, name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', views.groups_delete, name='groups_delete'),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
