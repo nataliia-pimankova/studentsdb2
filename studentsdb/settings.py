dbc135218cac536a5a6c3447f3cb3e1a000e36c2
natalya2487 <natalya2487@gmail.com>
2016-10-07 09:14:56 +0300

    add PORTAL_URL, student_proc

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentsdb.context_processors.students_proc',
             ],
         },
     },
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
