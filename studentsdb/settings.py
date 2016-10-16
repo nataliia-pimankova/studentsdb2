811164c4cb01f7dfacf7dfe3e3becebb77214b39
natalya2487 <natalya2487@gmail.com>
2016-10-16 16:15:01 +0300

    change mysql to postgesql & import DATABASES

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import db
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -71,19 +72,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'USER': 'students_db_user',
-        'PASSWORD': 'hjvfirf2487',
-        'NAME': 'students_db',
-    }
-}
+DATABASES = db.DATABASES
 
 
 # Password validation
