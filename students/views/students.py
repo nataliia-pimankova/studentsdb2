37269c86e240e6fb077473a51a49fc4347c7f5ec
natalya2487 <natalya2487@gmail.com>
2016-10-18 15:24:43 +0300

    without Paginator

--- students/views/students.py
+++ students/views/students.py
@@ -1,7 +1,6 @@
 # coding=utf8
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models import Student
 
@@ -17,20 +16,38 @@ def students_list (request):
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
 
-    #paginate students
-    paginator = Paginator(students, 3)
     page = request.GET.get('page')
-    try:
-        students = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        students = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999); deliver
-        # last page of results.
-        students = paginator.page(paginator.num_pages)
 
-    return render(request, 'students/students_list.html', {'students': students})
+    try:
+        page = int(page)
+    except(ValueError, TypeError):
+        # if 'home'
+        page = 1
+
+    per_page = 3
+    num_pages, remainder = divmod(Student.objects.count() , per_page)
+    if remainder :
+        num_pages +=1
+
+    if  page < 1:
+        page = 1
+    elif page > num_pages:
+        page = num_pages
+
+    start = (int(page)-1)*per_page
+    limit = start + per_page
+    students = Student.objects.all()[start: limit]
+
+    if num_pages > 1:
+        students.has_other_pages = True
+    else:
+        students.has_other_pages = False
+
+    students.page_range = range(1, num_pages+1)
+    students.page = page
+
+    return render(request, 'students/students_list.html',
+                  {'students': students })
 
 def students_add (request):
     return HttpResponse('<h1>Student Add Form</h1>')
