ca46c90e7d74f514b5d4aae284301e3fb4ec3ca3
natalya2487 <natalya2487@gmail.com>
2016-10-21 09:42:07 +0300

    add model Test

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models.Student import Student
 from .models.Group import Group
+from .models.Test import Test
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Group)
+admin.site.register(Test)
---
+++ students/models/Test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Test(models.Model):
+    """Test Model"""
+
+    class Meta(object):
+        verbose_name = u"Іспит"
+        verbose_name_plural = u"Іспити"
+
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Назва предмету"
+    )
+
+    date = models.DateField(
+        blank=False,
+        verbose_name=u"Дата і час проведення",
+        null=True
+    )
+
+    teacher = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Викладач"
+    )
+
+    group = models.ForeignKey('Group',
+        verbose_name=u"",
+        blank=False,
+        null=True,
+        on_delete=models.PROTECT
+    )
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки"
+    )
+
+    def __unicode__(self):
+        return u"%s (%s - %s)" % (self.title, self.group.title, self.teacher)
+
