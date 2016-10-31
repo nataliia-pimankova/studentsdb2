451c5c57e19f5ada22fb7ed6ea65d766a36bb60a
natalya2487 <natalya2487@gmail.com>
2016-10-31 16:01:00 +0200

    add validator form

--- students/views/students.py
+++ students/views/students.py
@@ -60,22 +60,51 @@ def students_add (request):
         # Якщо кнопка Додати була натиснута: was form add button clicked?
         if request.POST.get('add_button') is not None:
 
-            # TODO: validate input from user Перевіряємо дані на коректність та збираємо помилки
+            # errors collection
             errors ={}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),
+                    'notes': request.POST.get('notes')}
+
+            #validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов'язковою"
+            else:
+                data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Номер білета є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if student_group:
+                errors['student_group'] = u"Оберіть групу для студента"
+            else:
+                data['student_group'] = student_group
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
 
             # Якщо дані були введені коректно:
             if not errors:
                 # Створюємо та зберігаємо студента в базу
-                student = Student(
-                    first_name = request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                    notes = request.POST['notes'],
-                )
+                student = Student(**data)
                 # save it to database
                 student.save()
 
