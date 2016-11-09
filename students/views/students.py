b59ecec94733f633f3f640fddc1cbb3087076610
natalya2487 <natalya2487@gmail.com>
2016-11-09 09:57:17 +0200

    StudentUpdateForm with Crispy

--- students/views/students.py
+++ students/views/students.py
@@ -9,6 +9,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.forms import ModelForm
 from django.views.generic import UpdateView
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from ..models.Student import Student
 from ..models.Group import Group
 
@@ -151,24 +154,42 @@ class StudentUpdateForm(ModelForm):
         fields = "__all__"
 
     def __init__(self, *args, **kwargs):
+        # call original initializator
         super(StudentUpdateForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+
+        # set form tag attributes
+        self.helper.form_action = reverse('students_edit',
+                    kwargs={'pk': kwargs['instance'].id})
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        #form buttons
+        self.helper.add_input(Submit('add_button', u'Зберегти', css_class='btn btn-primary'))
+        self.helper.add_input(Submit('cancel_button', u'Скасувати', css_class='btn btn-link'))
+
 
 class StudentUpdateView(UpdateView):
     model = Student
-    template_name = 'students/students_edit.html'
     form_class = StudentUpdateForm
+    template_name = 'students/students_edit.html'
 
     def get_success_url(self):
-        return u'%s?status_message=Студента успішно збережено!' \
-            % reverse('home')
+        return u"%s?status_message=Студента успішно збережено!" % reverse('home')
 
     def post(self, request, *args, **kwargs):
         if request.POST.get('cancel_button'):
-            return HttpResponseRedirect(
-                u'%s?status_message=Редагування студента відмінено!' %
-                reverse('home'))
+            return HttpResponseRedirect(u"%s?status_message=Редагування студента відмінено!" % reverse('home'))
         else:
-            return super(StudentUpdateView, self).post(request, *args, **kwargs)
+            return super(StudentUpdateView, self).post(request,*args,**kwargs)
+
 
 
 def students_edit (request, sid):
