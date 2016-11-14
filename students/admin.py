f7a1da0337f935efa9f89c7d6972b57c8a48ebb0
natalya2487 <natalya2487@gmail.com>
2016-11-14 13:54:20 +0200

    custom students in admin

--- students/admin.py
+++ students/admin.py
@@ -1,11 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from .models.Student import Student
 from .models.Group import Group
 from .models.Test import Test
 from .models.Result import Result
 
+from django.core.urlresolvers import reverse
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name','ticket','notes']
+
+    def view_on_site(self,obj):
+        return reverse('students_edit', kwargs={'pk':obj.id})
+    
+
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Group)
 admin.site.register(Test)
 admin.site.register(Result)
