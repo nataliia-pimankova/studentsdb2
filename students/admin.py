6753a9eb3894f4d7d3478126770aedaacd12084f
natalya2487 <natalya2487@gmail.com>
2016-11-14 20:44:07 +0200

    validator by group from StudentAdmin

--- students/admin.py
+++ students/admin.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm, ValidationError
+
 from .models.Student import Student
 from .models.Group import Group
 from .models.Test import Test
 from .models.Result import Result
 
-from django.core.urlresolvers import reverse
+class StudentFormAdmin(ModelForm):
+
+    def clean_student_group(self):
+        """Check if student is leader in any group
+
+        If Yes, then ensure it's the same as selected group."""
+        # get group where current student is a leader
+        groups = Group.objects.filter(leader=self.instance)
+        if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+            raise ValidationError(u'Студент є старостою іншої групи.', code='invalid')
+
+        return self.cleaned_data['student_group']
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'ticket', 'student_group']
@@ -16,6 +30,7 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ['student_group']
     list_per_page = 10
     search_fields = ['last_name', 'first_name', 'middle_name','ticket','notes']
+    form = StudentFormAdmin
 
     def view_on_site(self,obj):
         return reverse('students_edit', kwargs={'pk':obj.id})
