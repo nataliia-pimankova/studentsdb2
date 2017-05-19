91a93ce14404fac42caa45968f8f93ab3452499f
Наталя Піманкова <natalya2487@gmail.com>
2017-05-19 14:44:57 +0300

    journal: blank for jQuery code

---
+++ students/static/js/main.js
@@ -0,0 +1,9 @@
+function initJournal() {
+    $('.day-box input[type="checkbox"]').click(function(event){
+        alert('test');
+    });
+}
+
+$(document).ready(function(){
+    initJournal();
+});
