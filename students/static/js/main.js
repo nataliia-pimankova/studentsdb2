117faa8ea30e1a8b36fc401b1435ffa794d93ae3
Наталя Піманкова <natalya2487@gmail.com>
2017-05-24 15:38:06 +0300

    add datepicker

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -51,7 +51,16 @@ function  initGroupSelector() {
     });
 }
 
+function initDateFields() {
+    $('input.dateinput').datetimepicker({
+        'format': 'YYYY-MM-DD'
+    }).on('dp.hide', function (event) {
+        $(this).blur();
+    });
+}
+
 $(document).ready(function(){
     initJournal();
     initGroupSelector();
+    initDateFields();
 });
